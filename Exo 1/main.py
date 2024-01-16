3d587c57c50b1c3ebe6316c64d447b1791b8a0e4
Rhlatech <tatayoyoh@mymail>
2024-01-16 11:26:24 +0100

    Finiolage du code pour l'exo

--- Exo 1/main.py
+++ Exo 1/main.py
@@ -4,29 +4,26 @@ from exo1_starter_template import *
 
 
 
-wall_e = Robot("wall_e")
+wall_e = Robot("wall_e", True, 150)
 print(wall_e.name)
 
 # Allume robot
-wall_e.power = True
-if(wall_e.power):
-    print("allume")
-
-#eteindre robot
-wall_e.power = False
-if(not wall_e.power):
-    print("Eteindre")
-
-wall_e.rechargement()
-    
-wall_e.current_speed = 50
-# Allume robot
-wall_e.power = True
-if(wall_e.power):
-    print("Allume")
-
+wall_e.running()
 print(wall_e)
 
+#eteindre robot
+wall_e.shutdown()
+print(wall_e)
+#wall_e.rechargement()
+    
+wall_e.current_speed = 50
+
+# Allume robot
+wall_e.running()
+
+wall_e.running()
+
+print("------------------")
 wall_e.stop()
 
 print(wall_e)
