a36aa73d3822b55c31f355fcbe031ea902658282
Rhlatech <tatayoyoh@mymail>
2024-01-16 16:04:57 +0100

    Mise en place du truc sympa

--- Exo_2/Cyborg.py
+++ Exo_2/Cyborg.py
@@ -11,43 +11,29 @@ class Cyborg(Robot, Humane):
         Humane.__init__(self, name, sex)
   
 
-  #Méthode qui va retourner toutes les info d'un robot sous format string
+    #Méthode qui va retourner toutes les info d'un cyborg sous format string
     def __str__(self):
         return "Nom du cyborg : {} / Sexe: {}".format(self.name, self.sexe)
     
+    #Méthode qui va retourner toutes les info Robot et humain fusionne   
     def status(self):
         return (Robot.__str__(self)+ " // "+Humane.__str__(self))
     
-    def truc_fun():
-        turns = 10
-        print ("Hello, Let's play hangman! You will have " + str(turns) + " turns!")
-        print("")
-        # delay
-        time.sleep(0.5)
-        # set of words to guess from
-        wordList = ["geekflare", "awesome", "python", "magic"]
-        word = random.choice(wordList)
-        guesses = ''
-        # loop till no turns are remaining
-        while turns > 0:         
-            wrong = 0             
-            for char in word:      
-                if char in guesses:    
-                    print(char),    
-                else:
-                    print("_"),     
-                    wrong += 1    
-            print("\n")
-            if wrong == 0:        
-                print ("You won :)")  
-                break             
-            guess = ''
-            if len(guess) < 1:
-                guess = raw_input("Guess a character or enter the correct word: ")[0]
-            guesses += guess                    
-            if guess not in word:  
-                turns -= 1        
-                print("Wrong")    
-                print("You have", + turns, ' turns left!') 
-                if turns == 0:           
-                    print("You Lose :(")
+    #Pti jeux sympa du cyborg
+    def truc_fun(self):
+        prix = random.randint(1, 10)
+        score = 100
+        tentatives = 0
+        print("Devinez le juste prix ! Le prix est un nombre compris entre 1 et 10 inclus.")
+        while True:
+            nombre = int(input())
+            tentatives += 1
+            if nombre < prix:
+                print("Le just prix est plus haut")
+            if nombre > prix:
+                print("Le juste prix est plus bas")
+            if nombre == prix:
+                print("Félicitations, vous avez trouvé le juste prix {} en {} essais, votre score est {} !".format(prix, tentatives, int(score / tentatives)))
+                print("Partie terminée")
+                break
+                
